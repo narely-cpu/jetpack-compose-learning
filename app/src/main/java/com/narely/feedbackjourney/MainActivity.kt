9d28f80ae8a80be2aa9731092fa2ff39e16c753d
Narely <narely@ciandt.com>
2026-06-22 16:52:11 -0300

    feat: create forms to create user without persistent

--- app/src/main/java/com/narely/feedbackjourney/MainActivity.kt
+++ app/src/main/java/com/narely/feedbackjourney/MainActivity.kt
@@ -1,21 +1,30 @@
 package com.narely.feedbackjourney
 
+import android.R.attr.onClick
 import android.annotation.SuppressLint
 import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
-import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.Composable
@@ -26,8 +35,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.narely.feedbackjourney.ui.theme.FeedbackJourneyTheme
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
-import org.jetbrains.annotations.TestOnly
+import com.composables.icons.codicons.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,7 +46,6 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FeedbackJourneyTheme {
-
                 CreateUserScreen()
             }
         }
@@ -49,6 +59,7 @@ fun CreateUserScreen() {
     val sheetState = rememberModalBottomSheetState()
     val scope = rememberCoroutineScope()
     var showBottomSheet by remember { mutableStateOf(false) }
+
     Scaffold(floatingActionButton = {
         ExtendedFloatingActionButton(
             text = { Text("Create User") },
@@ -60,14 +71,25 @@ fun CreateUserScreen() {
     }) { content ->
         if (showBottomSheet) {
             ModalBottomSheet(onDismissRequest = { showBottomSheet = false}, sheetState = sheetState) {
-                Button(onClick = {
-                    scope.launch { sheetState.hide() }.invokeOnCompletion {
-                        if (!sheetState.isVisible) {
-                            showBottomSheet = false
+                Row() {
+                    Button(onClick = {
+                        scope.launch { sheetState.hide() }.invokeOnCompletion {
+                            if (!sheetState.isVisible) {
+                                showBottomSheet = false
+                            }
                         }
+                    }) {
+                        Text("Close Sheet")
                     }
-                }) {
-                    Text("Close Sheet")
+                    Button(onClick = {
+                        scope.launch { sheetState.hide() }.invokeOnCompletion {
+                            if (!sheetState.isVisible) {
+                                showBottomSheet = false
+                            }
+                        }
+                    }) {
+                        Text("Save Forms")
+                    }
                 }
                 FormCreateEditUserScreen()
             }
@@ -75,13 +97,109 @@ fun CreateUserScreen() {
     }
 }
 
-
 @Composable
 fun FormCreateEditUserScreen() {
     Column() {
-        Text("Hello Modal")
-    }
+        TextInputForm("Name")
+        TextInputForm("Email")
+        TextInputForm("Password")
+        SelectedTypeUser()
+        SelectedPDMUser()
 
+    }
+}
+
+@Composable
+fun TextInputForm(label: String) {
+    val textFieldState = rememberTextFieldState(initialText = label)
+    TextField(
+        state = textFieldState,
+        label = { Text(label) }
+    )
+}
+
+@Composable
+fun ChooseUser(option: String) {
+    val textFieldState = rememberTextFieldState(initialText = "")
+    TextField(
+        state = textFieldState,
+        label = { Text(option) },
+        placeholder = { Text(option) },
+        readOnly = true,
+        enabled = false
+    )
+}
+
+@Composable
+fun MenuSelected(typeUser: String, onClick: () -> Unit) {
+    Row() {
+        ChooseUser(typeUser)
+        IconButton(onClick = onClick) {
+            Icon(
+                painterResource(R.drawable.codicons_ic_chevron_down),
+                contentDescription = null
+            )
+        }
+    }
+}
+
+@Composable
+fun DropDownUserSelected(expanded: Boolean, listItems: List<String>, onDismiss: () -> Unit, onClick: (String) -> Unit) {
+
+    DropdownMenu(
+        expanded = expanded,
+        onDismissRequest = onDismiss
+    ) {
+        listItems.forEach { option ->
+            DropdownMenuItem(
+                text = { Text(option) },
+                onClick = { onClick(option) }
+            )
+        }
+    }
+}
+
+@Composable
+fun SelectedTypeUser() {
+    var expanded by remember { mutableStateOf(false) }
+    var typeUser by remember { mutableStateOf("Choose a type") }
+    val menuTypeUser: List<String> = listOf("Admin", "Collaborator", "PDM")
+    Box() {
+        MenuSelected(
+            typeUser
+        ) { expanded = !expanded }
+
+        DropDownUserSelected(expanded,
+            menuTypeUser,
+            { expanded = false },
+            { option ->
+                typeUser = option
+                expanded = false
+            }
+        )
+    }
+}
+
+@Composable
+fun SelectedPDMUser() {
+    var expanded by remember { mutableStateOf(false) }
+    var typeUser by remember { mutableStateOf("Choose a PDM") }
+    val menuPDMEmail: List<String> = listOf("maria@ciandt.com", "joao@ciandt.com", "fernando@ciandt.com")
+
+    Box() {
+        MenuSelected(
+            typeUser
+        ) { expanded = !expanded }
+
+        DropDownUserSelected(expanded,
+            menuPDMEmail,
+            { expanded = false },
+            { option ->
+                typeUser = option
+                expanded = false
+            }
+        )
+    }
 }
 
 @Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
