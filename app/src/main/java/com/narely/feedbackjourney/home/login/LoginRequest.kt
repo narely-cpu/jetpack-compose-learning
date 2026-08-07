927d09b13aea5be502a04d63e136f2ccfd307069
Narely <narely@ciandt.com>
2026-08-07 16:57:38 -0300

    refactor: add clean arch on login

--- app/src/main/java/com/narely/feedbackjourney/home/login/LoginRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.narely.feedbackjourney.home.login
-
-data class LoginRequest(
-    val email: String,
-    val password: String
-)
--- app/src/main/java/com/narely/feedbackjourney/home/login/LoginResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.narely.feedbackjourney.home.login
-
-import com.google.gson.annotations.SerializedName
-
-data class LoginResponse(
-    @SerializedName("token") val token: String
-)
