30531ba6f90d3400afe54dcde3deb75fa87356cf
Narely <narely@ciandt.com>
2026-08-13 17:23:26 -0300

    tests: add getuserusecasetest

--- app/src/test/java/com/narely/feedbackjourney/createedituser/domain/GetUserUseCaseTest.kt
+++ app/src/test/java/com/narely/feedbackjourney/createedituser/domain/GetUserUseCaseTest.kt
@@ -15,62 +15,62 @@ import org.junit.Test
 import org.junit.jupiter.api.Assertions
 
 class GetUserUseCaseTest {
-    @MockK
-    private lateinit var usersRepositoryImpl: UsersRepositoryImpl
-
-    @InjectMockKs
-    private lateinit var getUserUseCase: GetUserUseCase
-
-    @Before
-    fun setup() {
-        MockKAnnotations.init(this)
-    }
-
-    @Test
-    fun `GIVEN an existing userId WHEN invoke() is called THEN validate the result as the correct user`() =
-        runTest {
-            // GIVEN
-            val userId = 1
-            val userResponse = UserResponse(
-                id = userId,
-                name = "savi",
-                email = "savi@ciandt.com",
-                type = UserTypeEnum.PDM.userValue,
-                pdmId = null,
-                active = true
-            )
-            val item = UserDataModel(
-                id = userResponse.id,
-                name = userResponse.name,
-                email = userResponse.email,
-                password = "Collaborator123!",
-                type = userResponse.type,
-                pdmEmail = null,
-            )
-
-            coEvery { usersRepositoryImpl.getListPdm() } returns listOf(userResponse)
-            coEvery { usersRepositoryImpl.getUser(userId) } returns userResponse
-
-            // WHEN
-            val result = getUserUseCase.invoke(userId)
-
-            // THEN
-            Assertions.assertEquals(item, result)
-        }
-
-    @Test
-    fun `GIVEN userId is null WHEN invoke() is called THEN validate that the result is null`() =
-        runTest {
-            // GIVEN
-            val incorrectId = 2
-
-            coEvery { usersRepositoryImpl.getListPdm() } returns emptyList()
-            coEvery { usersRepositoryImpl.getUser(incorrectId) } returns null
-
-            // WHEN
-            val result = getUserUseCase.invoke(incorrectId)
-
-            // THEN
-            Assertions.assertNull(result)
-        }
+//    @MockK
+//    private lateinit var usersRepositoryImpl: UsersRepositoryImpl
+//
+//    @InjectMockKs
+//    private lateinit var getUserUseCase: GetUserUseCase
+//
+//    @Before
+//    fun setup() {
+//        MockKAnnotations.init(this)
+//    }
+//
+//    @Test
+//    fun `GIVEN an existing userId WHEN invoke() is called THEN validate the result as the correct user`() =
+//        runTest {
+//            // GIVEN
+//            val userId = 1
+//            val userResponse = UserResponse(
+//                id = userId,
+//                name = "savi",
+//                email = "savi@ciandt.com",
+//                type = UserTypeEnum.PDM.userValue,
+//                pdmId = null,
+//                active = true
+//            )
+//            val item = UserDataModel(
+//                id = userResponse.id,
+//                name = userResponse.name,
+//                email = userResponse.email,
+//                password = "Collaborator123!",
+//                type = userResponse.type,
+//                pdmEmail = null,
+//            )
+//
+//            coEvery { usersRepositoryImpl.getListPdm() } returns listOf(userResponse)
+//            coEvery { usersRepositoryImpl.getUser(userId) } returns userResponse
+//
+//            // WHEN
+//            val result = getUserUseCase.invoke(userId)
+//
+//            // THEN
+//            Assertions.assertEquals(item, result)
+//        }
+//
+//    @Test
+//    fun `GIVEN userId is null WHEN invoke() is called THEN validate that the result is null`() =
+//        runTest {
+//            // GIVEN
+//            val incorrectId = 2
+//
+//            coEvery { usersRepositoryImpl.getListPdm() } returns emptyList()
+//            coEvery { usersRepositoryImpl.getUser(incorrectId) } returns null
+//
+//            // WHEN
+//            val result = getUserUseCase.invoke(incorrectId)
+//
+//            // THEN
+//            Assertions.assertNull(result)
+//        }
 }
---
+++ app/src/test/java/com/narely/feedbackjourney/features/createedituser/domain/GetUserUseCaseTest.kt
@@ -0,0 +1,73 @@
+package com.narely.feedbackjourney.features.createedituser.domain
+
+import com.narely.feedbackjourney.commons.data.remote.model.UserResponse
+import com.narely.feedbackjourney.features.createedituser.data.CreateEditUserRepositoryImpl
+import com.narely.feedbackjourney.features.createedituser.domain.mapper.toUserType
+import com.narely.feedbackjourney.features.createedituser.domain.model.UserDataModel
+import com.narely.feedbackjourney.features.createedituser.domain.model.UserTypeEnum
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import org.junit.jupiter.api.Assertions
+
+class GetUserUseCaseTest {
+    @MockK
+    private lateinit var createEditUserRepositoryImpl: CreateEditUserRepositoryImpl
+
+    @InjectMockKs
+    private lateinit var getUserUseCase: GetUserUseCase
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+    }
+
+    @Test
+    fun `GIVEN an existing userId WHEN invoke() is called THEN validate the result as the correct user`() =
+        runTest {
+            // GIVEN
+            val userId = 1
+            val userResponse = UserResponse(
+                id = userId,
+                name = "savi",
+                email = "savi@ciandt.com",
+                type = UserTypeEnum.PDM.name,
+                pdmId = null,
+                active = true
+            )
+            val item = UserDataModel(
+                id = userResponse.id,
+                name = userResponse.name,
+                email = userResponse.email,
+                type = userResponse.type.toUserType(),
+                pdmEmail = null,
+            )
+
+            coEvery { createEditUserRepositoryImpl.getUser(userId) } returns userResponse
+
+            // WHEN
+            val result = getUserUseCase.invoke(id = userId, listPdm = listOf(item))
+
+            // THEN
+            Assertions.assertEquals(item, result)
+        }
+
+    @Test
+    fun `GIVEN userId is null WHEN invoke() is called THEN validate that the result is null`() =
+        runTest {
+            // GIVEN
+            val incorrectId = 2
+
+            coEvery { createEditUserRepositoryImpl.getUser(incorrectId) } returns null
+
+            // WHEN
+            val result = getUserUseCase.invoke(incorrectId, listPdm = emptyList())
+
+            // THEN
+            Assertions.assertNull(result)
+        }
+}
