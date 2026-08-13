4df77338c5405c4ebec13654d539cd80b0ee4614
Narely <narely@ciandt.com>
2026-08-13 15:18:47 -0300

    tests: add edituserusecasetest

--- app/src/test/java/com/narely/feedbackjourney/createedituser/domain/EditUserUseCaseTest.kt
+++ app/src/test/java/com/narely/feedbackjourney/createedituser/domain/EditUserUseCaseTest.kt
@@ -15,44 +15,44 @@ import org.junit.Test
 
 class EditUserUseCaseTest {
 
-    @MockK
-    private lateinit var usersRepositoryImpl: UsersRepositoryImpl
-
-    @InjectMockKs
-    private lateinit var editUserUseCase: EditUserUseCase
-
-    @Before
-    fun setup() {
-        MockKAnnotations.init(this)
-    }
-
-    @Test
-    fun `GIVEN modified user information WHEN invoke() is called THEN validate that the repository's update function is called`() =
-        runTest {
-            //GIVEN
-            val userId = 1
-            val request = UpdateUserRequest(
-                name = "saviolli",
-                email = "savi@ciandt.com",
-                type = UserTypeEnum.PDM.userValue,
-                pdmId = null
-            )
-
-            coEvery { usersRepositoryImpl.getListPdm() } returns emptyList()
-            coJustRun { usersRepositoryImpl.updateUser(id = userId, request = request) }
-
-            //WHEN
-            editUserUseCase.invoke(
-                id = userId,
-                name = request.name,
-                email = request.email,
-                userType = request.type,
-                pdmEmail = null,
-                finishedActivityCreateUser = {},
-                errorMessage = {}
-            )
-
-            //THEN
-            coVerify { usersRepositoryImpl.updateUser(id = userId, request = request) }
-        }
+//    @MockK
+//    private lateinit var usersRepositoryImpl: UsersRepositoryImpl
+//
+//    @InjectMockKs
+//    private lateinit var editUserUseCase: EditUserUseCase
+//
+//    @Before
+//    fun setup() {
+//        MockKAnnotations.init(this)
+//    }
+//
+//    @Test
+//    fun `GIVEN modified user information WHEN invoke() is called THEN validate that the repository's update function is called`() =
+//        runTest {
+//            //GIVEN
+//            val userId = 1
+//            val request = UpdateUserRequest(
+//                name = "saviolli",
+//                email = "savi@ciandt.com",
+//                type = UserTypeEnum.PDM.userValue,
+//                pdmId = null
+//            )
+//
+//            coEvery { usersRepositoryImpl.getListPdm() } returns emptyList()
+//            coJustRun { usersRepositoryImpl.updateUser(id = userId, request = request) }
+//
+//            //WHEN
+//            editUserUseCase.invoke(
+//                id = userId,
+//                name = request.name,
+//                email = request.email,
+//                userType = request.type,
+//                pdmEmail = null,
+//                finishedActivityCreateUser = {},
+//                errorMessage = {}
+//            )
+//
+//            //THEN
+//            coVerify { usersRepositoryImpl.updateUser(id = userId, request = request) }
+//        }
 }
---
+++ app/src/test/java/com/narely/feedbackjourney/features/createedituser/domain/EditUserUseCaseTest.kt
@@ -0,0 +1,63 @@
+package com.narely.feedbackjourney.features.createedituser.domain
+
+import com.narely.feedbackjourney.features.createedituser.data.CreateEditUserRepositoryImpl
+import com.narely.feedbackjourney.features.createedituser.data.remote.model.CreateEditUserRequest
+import com.narely.feedbackjourney.features.createedituser.domain.model.UserDataModel
+import com.narely.feedbackjourney.features.createedituser.domain.model.UserTypeEnum
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coJustRun
+import io.mockk.coVerify
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.*
+
+class EditUserUseCaseTest {
+
+    @MockK
+    private lateinit var createEditUserRepositoryImpl: CreateEditUserRepositoryImpl
+
+    @InjectMockKs
+    private lateinit var editUserUseCase: EditUserUseCase
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+    }
+
+    @Test
+    fun `GIVEN modified user information WHEN invoke() is called THEN validate that the repository's update function is called`() =
+        runTest {
+            //GIVEN
+            val userId = 1
+            val request = CreateEditUserRequest(
+                name = "saviolli",
+                email = "savi@ciandt.com",
+                type = UserTypeEnum.PDM.name,
+                pdmId = null
+            )
+            val collaboratorUser = UserDataModel(
+                id = userId,
+                name = "savi",
+                email = "savi@ciandt.com",
+                type = UserTypeEnum.PDM,
+                pdmEmail = null,
+            )
+
+            coJustRun { createEditUserRepositoryImpl.updateUser(id = userId, request = request) }
+
+            //WHEN
+            editUserUseCase.invoke(
+                collaborator = collaboratorUser,
+                pdm = null,
+                finishedActivityCreateUser = {},
+                errorMessage = {}
+            )
+
+            //THEN
+            coVerify { createEditUserRepositoryImpl.updateUser(id = userId, request = request) }
+        }
+}
