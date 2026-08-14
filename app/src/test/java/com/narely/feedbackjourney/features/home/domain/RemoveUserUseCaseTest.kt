a3eee9d3a63ecafe9068308d0237f7cabd42ccc7
Narely <narely@ciandt.com>
2026-08-14 15:11:23 -0300

    tests: add removeuserusecasetest

---
+++ app/src/test/java/com/narely/feedbackjourney/features/home/domain/RemoveUserUseCaseTest.kt
@@ -0,0 +1,39 @@
+package com.narely.feedbackjourney.features.home.domain
+
+import com.narely.feedbackjourney.features.home.data.HomeRepositoryImpl
+import io.mockk.MockKAnnotations
+import io.mockk.coJustRun
+import io.mockk.coVerify
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+class RemoveUserUseCaseTest {
+    @MockK
+    private lateinit var homeRepositoryImpl: HomeRepositoryImpl
+
+    @InjectMockKs
+    private lateinit var removeUserUseCase: RemoveUserUseCase
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+    }
+
+    @Test
+    fun `GIVEN the userId of the user to be removed WHEN invoke() is called THEN validate that the repository's remove function is called`() =
+        runTest {
+            // GIVEN
+            val userId = 1
+
+            coJustRun { homeRepositoryImpl.removeUser(userId) }
+
+            // WHEN
+            removeUserUseCase.invoke(userId)
+
+            // THEN
+            coVerify { homeRepositoryImpl.removeUser(userId) }
+        }
+}
--- app/src/test/java/com/narely/feedbackjourney/home/domain/RemoveUserUseCaseTest.kt
+++ app/src/test/java/com/narely/feedbackjourney/home/domain/RemoveUserUseCaseTest.kt
@@ -12,29 +12,29 @@ import org.junit.Before
 import org.junit.Test
 
 class RemoveUserUseCaseTest {
-    @MockK
-    private lateinit var usersRepositoryImpl: UsersRepositoryImpl
-
-    @InjectMockKs
-    private lateinit var removeUserUseCase: RemoveUserUseCase
-
-    @Before
-    fun setup() {
-        MockKAnnotations.init(this)
-    }
-
-    @Test
-    fun `GIVEN the userId of the user to be removed WHEN invoke() is called THEN validate that the repository's remove function is called`() =
-        runTest {
-            // GIVEN
-            val userId = 1
-
-            coJustRun { usersRepositoryImpl.removeUser(userId) }
-
-            // WHEN
-            removeUserUseCase.invoke(userId)
-
-            // THEN
-            coVerify { usersRepositoryImpl.removeUser(userId) }
-        }
+//    @MockK
+//    private lateinit var usersRepositoryImpl: UsersRepositoryImpl
+//
+//    @InjectMockKs
+//    private lateinit var removeUserUseCase: RemoveUserUseCase
+//
+//    @Before
+//    fun setup() {
+//        MockKAnnotations.init(this)
+//    }
+//
+//    @Test
+//    fun `GIVEN the userId of the user to be removed WHEN invoke() is called THEN validate that the repository's remove function is called`() =
+//        runTest {
+//            // GIVEN
+//            val userId = 1
+//
+//            coJustRun { usersRepositoryImpl.removeUser(userId) }
+//
+//            // WHEN
+//            removeUserUseCase.invoke(userId)
+//
+//            // THEN
+//            coVerify { usersRepositoryImpl.removeUser(userId) }
+//        }
 }
