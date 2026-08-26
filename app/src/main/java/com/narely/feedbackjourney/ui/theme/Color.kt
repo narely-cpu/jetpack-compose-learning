638cb73acd22ef57f364511aeb3bb5ba3e50f43f
Narely <narely@ciandt.com>
2026-08-26 17:32:05 -0300

    feat: added colors and text styles

--- app/src/main/java/com/narely/feedbackjourney/ui/theme/Color.kt
+++ app/src/main/java/com/narely/feedbackjourney/ui/theme/Color.kt
@@ -2,10 +2,26 @@ package com.narely.feedbackjourney.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Blue40 = Color(0xFFB4DCFA)
+val Blue80 = Color(0xFF2C5282)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Grey40 = Color(0xFFE6E6EE)
+val Grey80 = Color(0xFF8E8E8E)
+
+val Magenta80 = Color(0xFFA34A87)
+
+val Orange80 = Color(0xFFFA5A50)
+
+val Purple80 = Color(0xFF485794)
+//val Purple80 = Color(0xFFD0BCFF)
+//val PurpleGrey80 = Color(0xFFCCC2DC)
+//val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFFB6BCD4)
+//val PurpleGrey40 = Color(0xFF625b71)
+//val Pink40 = Color(0xFF7D5260)
+
+
+
+
+
