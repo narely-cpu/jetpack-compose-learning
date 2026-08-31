be2b69a6ee8d5810749866e07c8fed2b7110061a
Narely <narely@ciandt.com>
2026-08-31 16:40:56 -0300

    task: refactor home layout and add nagivation login to home via activity
    # Conflicts:
    #	app/src/main/java/com/narely/feedbackjourney/features/login/ui/LoginScreen.kt
    #	app/src/main/res/values/strings.xml
    
    # Conflicts:
    #	app/build.gradle.kts
    #	app/src/main/java/com/narely/feedbackjourney/commons/ui/EnterpriseLogo.kt
    #	app/src/main/java/com/narely/feedbackjourney/features/home/ui/HomeScreen.kt
    #	app/src/main/java/com/narely/feedbackjourney/features/login/ui/LoginScreen.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,6 +75,7 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.gson)
     implementation(libs.converter.gson)
+    implementation(libs.icons.heroicons.outline.android)
     implementation(libs.androidx.compose.material.icons.extended)
     ksp(libs.hilt.compiler)
     testImplementation(libs.junit)
