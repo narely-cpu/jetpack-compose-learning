fcdf60357428567ab9c25487fba31143c617c5a7
Narely <narely@ciandt.com>
2026-09-01 18:01:32 -0300

    task: create managementuserviewmodel and config managementuserapi

--- app/src/main/java/com/narely/feedbackjourney/features/home/domain/model/MyListItem.kt
+++
@@ -1,5 +0,0 @@
-package com.narely.feedbackjourney.features.home.domain.model
-
-import androidx.compose.ui.graphics.vector.ImageVector
-
-data class MyListItem(val onClick: () -> Unit, val painterId: Int, val contentDescription: Int)
