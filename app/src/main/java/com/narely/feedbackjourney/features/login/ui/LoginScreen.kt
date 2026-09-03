4ed82b92c3f15bdfb3617ad4616ec7e41df48fb5
Narely <narely@ciandt.com>
2026-09-03 17:05:35 -0300

    bugfix: fix dropdown and add collaborator button

--- app/src/main/java/com/narely/feedbackjourney/features/login/ui/LoginScreen.kt
+++ app/src/main/java/com/narely/feedbackjourney/features/login/ui/LoginScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -81,6 +82,9 @@ private fun FormLoginLayout(
                     placeholder = null,
                     valueState = userEmail,
                     trailingIcon = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(50.dp),
                     updateValueState = onUserEmailChange
                 )
                 TextInputForm(
@@ -88,6 +92,9 @@ private fun FormLoginLayout(
                     placeholder = null,
                     valueState = userPassword,
                     trailingIcon = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(50.dp),
                     updateValueState = onUserPasswordChange
                 )
             }
