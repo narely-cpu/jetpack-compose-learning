112320cd1f6aaf53f06e2e87efbfe1b4e53370f3
Narely <narely@ciandt.com>
2026-09-10 15:23:17 -0300

    chore: removal of unused code

--- app/src/main/java/com/narely/feedbackjourney/features/home/HomeActivity.kt
+++ app/src/main/java/com/narely/feedbackjourney/features/home/HomeActivity.kt
@@ -4,23 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import com.narely.feedbackjourney.features.home.ui.HomeScreen
-import com.narely.feedbackjourney.features.home.ui.HomeViewModel
 import com.narely.feedbackjourney.ui.theme.FeedbackJourneyTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeActivity : ComponentActivity() {
 
-    val homeViewModel: HomeViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             FeedbackJourneyTheme {
-                HomeScreen(viewModel = homeViewModel)
+                HomeScreen()
             }
         }
     }
