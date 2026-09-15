afad8c6f035543574c19f838fd62d9002a39d9e2
Narely <narely@ciandt.com>
2026-09-15 17:30:49 -0300

    refactor: remove height and add padding

--- app/src/main/java/com/narely/feedbackjourney/features/login/ui/LoginScreen.kt
+++ app/src/main/java/com/narely/feedbackjourney/features/login/ui/LoginScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -81,9 +80,7 @@ private fun FormLoginLayout(
                     placeholder = null,
                     valueState = userEmail,
                     trailingIcon = null,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(50.dp),
+                    modifier = Modifier.fillMaxWidth(),
                     updateValueState = onUserEmailChange
                 )
                 TextInputFormComponent(
@@ -91,9 +88,7 @@ private fun FormLoginLayout(
                     placeholder = null,
                     valueState = userPassword,
                     trailingIcon = null,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(50.dp),
+                    modifier = Modifier.fillMaxWidth(),
                     updateValueState = onUserPasswordChange
                 )
             }
